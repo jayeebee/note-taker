0952852dc1e3ff0a49b6e32aadf222fd07829fda
jayeebee <pnuthexsa311@gmail.com>
2021-05-02 19:10:10 -0400

    js errors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const { notes } =require("./db/db.json")
 
-app.use(express.static('public'));
+app.use(express.static('/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
