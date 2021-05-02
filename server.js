5ce267c9a939ba3765493e1e82225711aa18349b
jayeebee <pnuthexsa311@gmail.com>
2021-05-02 17:43:19 -0400

    intial

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const { notes } =require("./db/db.json")
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -21,3 +22,9 @@ app.get("/api/notes", (req, res) => {
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
  });
+
+
+
+ app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
