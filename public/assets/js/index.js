a394d3807f683d9fe41fdee160ad548499c68c6b
jayeebee <pnuthexsa311@gmail.com>
2021-05-02 19:29:14 -0400

    js errors

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -26,7 +26,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes', {
+  fetch('/api/Notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
