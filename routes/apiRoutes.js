614ab12e33e99b6165e6666a8991f712b320f8f9
jayeebee <pnuthexsa311@gmail.com>
2021-05-02 20:27:18 -0400

    semi routing, getting errors

---
+++ routes/apiRoutes.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const router = require('express').Router();
+const { notes } = require("../db/db.json");
+
+
+router.get("/notes", (req, res) => {
+    let input = JSON.parse(fs.readFileSync("../db/db.json", "utf8"))
+    res.json(input);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    const note = createNewNote(req.body, notes)
+    res.json(note)
+});
+
+
+function createNewNote (body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({ notes: notesArray}, null, 2)
+    )
+    return note;
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,45 +1,19 @@
 const express = require('express');
-const fs = require("fs");
-const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const { notes } = require("./db/db.json")
 
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', apiRoutes)
+app.use('/', htmlRoutes)
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("/api/notes", (req, res) => {
-    let input = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
-    res.json(input);
-});
-
-app.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes)
-    res.json(note)
-});
-
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
- });
-
-function createNewNote (body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray}, null, 2)
-    )
-    return note;
-}
-
-
- app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
