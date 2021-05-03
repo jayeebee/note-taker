050f4d320286afe3570f7e001c39274497604f64
jayeebee <pnuthexsa311@gmail.com>
2021-05-02 20:53:29 -0400

    fixed error, notes show and can be psoted

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,10 @@ const path = require("path");
 const router = require('express').Router();
 const { notes } = require("../db/db.json");
 
-
+router.get('/notes', (req, res) => {
+    res.json(notes);
+  });
+  
 router.get("/notes", (req, res) => {
     let input = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
     res.json(input);
