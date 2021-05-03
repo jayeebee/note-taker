16e0003488a69340dc63374ddc433cc008148b49
jayeebee <pnuthexsa311@gmail.com>
2021-05-02 20:43:37 -0400

    more routing, directory fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,7 @@ const { notes } = require("../db/db.json");
 
 
 router.get("/notes", (req, res) => {
-    let input = JSON.parse(fs.readFileSync("../db/db.json", "utf8"))
+    let input = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
     res.json(input);
 });
 
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const { notes } = require("./db/db.json")
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
